74c9927c6b5fafefec39f9523411117c26de78d6
smfaizulhoda3772@gmail.com <smfaizulhoda3772@gmail.com>
2023-01-31 10:30:04 +0530

    commit

---
+++ automatictypeconversion.js
@@ -0,0 +1,11 @@
+var a="5"+1;
+console.log(a);
+let b="sa"+2;
+console.log(b)
+var a="5"-1;
+console.log(a);
+console.log(undefined*3);
+console.log(null*10);
+console.log("ten"*3);
+console.log("10"*3);
+// run code and the tthe magic
